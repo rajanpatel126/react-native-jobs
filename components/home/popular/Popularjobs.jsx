aa1a34d5bf0d5b0b64cde46d541dd2b4b39bbc97
Rajan Patel <88902043+rajanpatel126@users.noreply.github.com>
2023-12-19 13:02:27 +0530

    Moving to Jobs details section.

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -21,7 +21,7 @@ const Popularjobs = () => {
     query: "React developer",
     num_pages: "1",
   });
-  console.log(data);
+  // console.log(data);
   return (
     <View style={styles.container}>
       <View style={styles.header}>
